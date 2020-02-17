b0acc16656c7bd0d4990d8c8fb772a1d30bdf8b4
grasevesper <61079420+grasevesper@users.noreply.github.com>
2020-02-17 20:11:37 +0600

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,21 @@
+'''Write a program that greets the user by printing the word "Hello", a comma, the
+name of the user and an exclamation mark after it. See the examples below.
+Warning. Your program's output should strictly match the desired one,
+character by character. There shouldn't be any space between the name and
+the exclamation mark. You can use + operator to concatenate two strings.
+'''
+
+# names = ['Max', 'Erica', 'Azamat']
+# for name in names:
+#     print('Hello' + ', ' + name + '!')
+
+# def func(x): return x-1 
+# print(func(3)*func(5))
+
+x = 3 if 2>x: 
+print(‘First’) 
+else: 
+    print(‘Second’) 
+if 2>x: 
+    print(‘Third’) 
+print(‘Fourth’) print(‘Fifth’)
